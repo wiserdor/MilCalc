06c6b2c4680466193afebb97f1d48831b714bb02
Dor Wiser <vgibsonsg@gmail.com>
2024-01-08 16:52:11 +0200

    remove noti

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -34,10 +34,10 @@ const Header = () => {
       <div className={styles.pageTitle}>מחשבון מענקים</div>
       <div className={styles.pageTitle}>״מגויסים למילואים״</div>
       <div className={styles.subTitle}>מלחמת חרבות ברזל</div>
-      <div className={styles.importantNotice}>
+      {/* <div className={styles.importantNotice}>
         ברגעים אלו אנו מעדכנים את המחשבון בהתאם לאישורי הממשלה. ממליצים להיכנס
         שוב ולהתעדכן מחר
-      </div>
+      </div> */}
       <p className={styles.descriptionIntro}>
         בעזרת מחשבון זה תוכלו לחשב מה צפויים להיות המענקים שתקבלו מהמדינה עבור
         שירות המילואים במלחמת חרבות ברזל.
