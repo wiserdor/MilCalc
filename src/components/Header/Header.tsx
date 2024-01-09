e26c5d1306947f1a7bb04533bbb903ab985bd48f
Dor Wiser <vgibsonsg@gmail.com>
2024-01-09 12:52:46 +0200

    update notice

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -64,11 +64,11 @@ const Header = () => {
           <XIcon size={32} round={true} />
         </TwitterShareButton>
       </div>
-      <div className={styles.importantNotice}>
+      {/* <div className={styles.importantNotice}>
         <div>⚠️</div>
         היי חברים מילואימניקים, שוב יצאה אגרת חדשה ולכן ייתכנו אי דיוקים... אנו
         עובדים לעדכן את המחשבון עם הפרטים החדשים בהקדם האפשרי.
-      </div>
+      </div> */}
       <p className={styles.descriptionIntro}>
         בעזרת מחשבון זה תוכלו לחשב מה צפויים להיות המענקים שתקבלו מהמדינה עבור
         שירות המילואים במלחמת חרבות ברזל.
