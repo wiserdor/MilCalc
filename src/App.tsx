3be9b6c4db2626762614673c230011ff62efa71a
Dor Wiser <wiserdor@gmail.com>
2024-01-10 12:40:39 +0200

    addons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer'
 import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 import Results from './components/Results/Results'
+import RiseupBanner from './components/RiseupBanner/RiseupBanner'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Form />
       <Results />
+      <RiseupBanner />
       <FAQ />
       <Footer />
     </div>
