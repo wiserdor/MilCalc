7f631610a2ac1d32a98a0474567b8ff2a52e52b2
Tal BenAvra <42270952+AvraCodabra@users.noreply.github.com>
2024-01-12 10:18:29 +0200

    Update link to IDF information page

--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -1,6 +1,7 @@
 import styles from './Disclaimer.module.css'
 
 const Disclaimer = () => {
+  const idfInfoLink = "https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/"
   return (
     <div className={styles.disclaimer}>
       <div>
@@ -10,7 +11,7 @@ const Disclaimer = () => {
         </div>
         המחשבון מבוסס על{' '}
         <a
-          href="https://usms.cellcom.co.il/files/QEIKC4/khscgxsw2z.pdf"
+          href={idfInfoLink}
           target="_blank"
           rel="noopener noreferrer"
         >
