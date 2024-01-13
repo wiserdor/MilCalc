2029ccd5ee3b8278b3ca841bb0aa9f6878357f8e
Dor Wiser <vgibsonsg@gmail.com>
2024-01-13 17:42:25 +0200

    chatgpt bot

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -2,13 +2,41 @@ import Accordion from '../Accordion/Accordion'
 import style from './FAQ.module.css'
 import { faqQuestions } from './questions'
 
-
-
 const FAQ = () => {
   return (
     <div className={style.FAQ}>
       <div className={style.FAQTitle}>שאלות ותשובות:</div>
       <Accordion items={faqQuestions}></Accordion>
+      <div style={{ marginTop: 8, textAlign: 'center' }}>
+        <a
+          href="https://chat.openai.com/g/g-SyphjNdxN-myd-tgmvlym-mlkhmt-khrbvt-brzl"
+          target="_blank"
+          rel="noreferrer"
+          style={{
+            textAlign: 'center',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 8,
+          }}
+        >
+          <img src="/svg/chatgpt.svg" style={{ width: '100%' }} />
+          חדש! בוט שאלות ותשובות
+          <br />
+          לחצו כאן כדי להתחיל לשוחח
+        </a>
+        <div
+          style={{
+            color: '#767676',
+          }}
+        >
+          <br />
+          *הבוט הוא נסיוני וכרגע עבור משתמשי <br />
+          ChatGPT Plus.
+          <br /> ננסה למצוא פתרון שיהיה זמין לכולם ככל שיהיה ניתן בעתיד.
+        </div>
+      </div>
     </div>
   )
 }
