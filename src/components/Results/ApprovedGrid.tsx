310bb38cd9b75ab78f395fb13b46edac9b70fdad
Dor Wiser <vgibsonsg@gmail.com>
2024-01-13 23:21:00 +0200

    description

--- src/components/Results/ApprovedGrid.tsx
+++ src/components/Results/ApprovedGrid.tsx
@@ -39,43 +39,63 @@ const ApprovedGrid = (props: ApprovedGridProps) => {
       {
         name: 'התגמול הנוסף',
         totalCompensation: totalAdditional,
+        description:
+          'ישולם ב- 1/5/24 באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
       {
         name: 'התגמול המיוחד',
         totalCompensation: totalSpecialDays + totalExtended,
+        description:
+          'ישולם ב- 1/5/24 באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
 
       {
         name: 'הוצאות אישיות (5 ימים רצופים).',
         totalCompensation: totalDaysStraight,
+        description:
+          'ישולם ב- 1/5/24 באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
       {
         name: 'מענק הוצאות אישיות מוגדל',
         totalCompensation: totalPerMonthApproved,
+        description:
+          'ישולם בפעימה ראשונה עד ה- 14/1/24 (עבור השמ"פ שבוצע ב-2023), באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
       {
         name: 'מענק משפחה מוגדל',
         totalCompensation: totalFromChildrenApproved,
+        description:
+          'ישולם בפעימה ראשונה עד ה- 14/1/24 (עבור השמ"פ שבוצע ב-2023), באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
       {
         name: 'מענק כלכלת בית מוגדל',
         totalCompensation: totalMoreThan45,
+        description:
+          'ישולם ב- 1/9/24 באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל. עשוי להגיע בפעימה נוספת ב- 1/12/24.',
       },
       {
         name: 'מענק משפחה מיוחדת',
         totalCompensation: totalSpecialChildren,
+        description:
+          'ישולם עד 1/3/24 באופן אוטומטי וחד פעמי (ובכפוף לקבלת המידע הנדרש ממשרדי הממשלה) ישירות לחשבון הבנק המדווח במערכות צה״ל.',
       },
       {
         name: 'שובר חופשה',
         totalCompensation: totalVacation,
+        description:
+          'שובר אישי יישלח במהלך חציון ב׳ 2024 באופן אוטומטי כהודעה לטלפון הנייד של חייל המילואים, למימוש עד סוף שנת 2026.',
       },
       {
         name: 'טיפול זוגי',
         totalCompensation: totalFamilyCare,
+        description:
+          'הסיוע יינתן בכפוף לתצהיר + קבלה שיגיש משרת המילואים ולא יותר מהתשלום בפועל. הגשת קבלת החזר החל מ- 1/2/24 לאחר שליחת מסרון פרטני. לניצול עד סוף 2026.',
       },
       {
         name: 'טיפול רגשי, נפשי ומשלים',
         totalCompensation: totalMental,
+        description:
+          'הסיוע יינתן בכפוף לתצהיר + קבלה שיגיש משרת המילואים ולא יותר מהתשלום בפועל. הגשת קבלת החזר החל מ- 1/2/24 לאחר שליחת מסרון פרטני. לניצול עד סוף 2026.',
       },
     ],
     [props]
