f3b6e154cfc6bb0ad5548daeb4a7071185964f39
Dor Wiser <wiserdor@gmail.com>
2024-01-16 09:47:34 +0200

    remove git banner

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -21,32 +21,6 @@ const Footer = () => {
           נשמח לשמוע מכם בכל נושא שקשור למחשבון זה. ניתן ליצור קשר במייל:
           <a href="mailto:yslook7@gmail.com">yslook7@gmail.com</a>
         </p>
-
-        <div style={{ marginTop: 32 }}>מתכנתים? תרמו למיזם {'-->  '} </div>
-        <div>
-          <a
-            href="https://github.com/wiserdor/MilCalc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: 4,
-              }}
-            >
-              <div>/https://github.com/wiserdor/MilCalc</div>
-              <img
-                width={24}
-                height={24}
-                src="/svg/github-mark.svg"
-                alt="github"
-              />
-            </div>
-          </a>
-        </div>
       </div>
     </footer>
   )
