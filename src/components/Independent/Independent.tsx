a3294e16585d5b0ef56f01f5b9915508b31f99de
Dor Wiser <vgibsonsg@gmail.com>
2024-01-22 23:16:42 +0200

    independant new style

--- src/components/Independent/Independent.tsx
+++ src/components/Independent/Independent.tsx
@@ -12,78 +12,49 @@ const Independent = () => {
   const { title, description, imgUrl, links, phone } = data[0]
 
   return (
-    <div className={style.independent}>
-      <div className={style.header}>
-        <h2 className={style.title}>העסק של מילואימניק</h2>
-        <div className={style.subtitle}>
-          חשוב לנו לעזור לעצמאיים שהעסק שלהם נפגע בעקבות המלחמה. רוצים להופיע?
-          דברו איתנו במייל:{' '}
-          <a href="mailto:yslook7@gmail.com">yslook7@gmail.com</a>
-        </div>
-      </div>
-      <div className={style.imgContainer}>
-        <img className={style.img} src={`/independent/${imgUrl}`} />
-      </div>
-      <h3 className={style.business}>{title}</h3>
-      <div className={style.description}>{description}</div>
-      <div className={style.links}>
-        אתר העסק:{' '}
-        {links.map((link) => (
-          <div key={link.url}>
-            <a
-              href={link.url}
-              target="_blank"
-              rel="noreferrer"
-              className={style.link}
-            >
-              {link.text}
-            </a>
+    <div style={{ marginTop: 12, width: '100%' }}>
+      <h2 className={style.title}>עצמאים במילואים</h2>
+      <div className={style.independent}>
+        <div
+          style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+        >
+          <div className={style.imgContainer}>
+            <img className={style.img} src={`/independent/${imgUrl}`} />
           </div>
-        ))}
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          gap: 8,
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            gap: 8,
-          }}
-        >
-          <div>טלפון: </div>
-          <a href={`tel:${phone}`} className={style.phone}>
-            {toPhoneString(phone)}
-            <span style={{ fontSize: 18 }}>{'  '}📞 </span>
-          </a>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            gap: 8,
-          }}
-        >
-          <div>וואטסאפ: </div>
+        <h3 className={style.business}>{title}</h3>
+        <div className={style.description}>{description}</div>
+        <div className={style.contact}>
+          <a href={`tel:${phone}`}>
+            <div className={style.contactColumn}>
+              <img src="/svg/phone.svg" width={16} />
+              <div>{toPhoneString(phone)}</div>
+            </div>
+          </a>
           <a
             href={`https://api.whatsapp.com/send?phone=972${phone.substring(
               1
             )}`}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img
-              src="/svg/whatsapp.svg"
-              width={30}
-              className={style.whatsapp}
-            />
+            <div className={style.contactColumn}>
+              <img src="/svg/whatsapp.svg" width={16} />
+              <div>ווצסאפ</div>
+            </div>
+          </a>
+          <a href={links[0].url} target="_blank" rel="noopener noreferrer">
+            <div className={style.contactColumn}>
+              <img src="/svg/click.svg" width={16} />
+              <div>{links[0].text}</div>
+            </div>
           </a>
         </div>
+      </div>
+      <div className={style.subtitle}>
+        חשוב לנו לעזור לעצמאיים שהעסק שלהם נפגע בעקבות המלחמה. רוצים להופיע?
+        דברו איתנו במייל:{' '}
+        <a href="mailto:yslook7@gmail.com">yslook7@gmail.com</a>
       </div>
     </div>
   )
