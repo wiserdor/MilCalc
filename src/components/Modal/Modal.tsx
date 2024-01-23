1dc7526c8a5cca47501e89ec078fd3780bbc6e50
Dor Wiser <wiserdor@gmail.com>
2024-01-23 13:01:16 +0200

    animation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,33 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import style from './Modal.module.css'
 
 export interface ModalProps {
   children: React.ReactNode
   isOpen: boolean
+
   handleBackdropClick?: () => void
 }
 
 const Modal = (props: ModalProps) => {
   const { children, isOpen, handleBackdropClick } = props
 
-  if (!isOpen) return null
+  if (!isOpen) return <AnimatePresence />
 
   return (
-    <div className={`${style.modalWrapper}`}>
-      <div className={style.modalBackdrop} onClick={handleBackdropClick} />
-      <dialog className={style.modal} open={isOpen}>
-        <div className={style.modalChild}>{children}</div>
-      </dialog>
-    </div>
+    <AnimatePresence>
+      <div className={`${style.modalWrapper}`}>
+        <motion.div
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <div className={style.modalBackdrop} onClick={handleBackdropClick} />
+          <dialog className={style.modal} open={isOpen}>
+            <div className={style.modalChild}>{children}</div>
+          </dialog>
+        </motion.div>
+      </div>
+    </AnimatePresence>
   )
 }
 
