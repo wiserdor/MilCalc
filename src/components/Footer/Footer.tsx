4549aacd0206c377b1cadce0f8a4410d46819766
Dor Wiser <wiserdor@gmail.com>
2024-01-25 17:41:49 +0200

    fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,15 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: 6,
+        }}
+      >
         <div
           style={{
             color: 'white',
@@ -16,29 +24,23 @@ const Footer = () => {
         >
           לתרומה למיזם:
         </div>
+
+        <BuyMeACoffeeButton />
+        <PaypalDonateButton />
+
         <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: 6,
-          }}
-        >
-          <BuyMeACoffeeButton />
-          <PaypalDonateButton />
-        </div>
-        <p
           style={{
             color: 'white',
             fontSize: 14,
+            width: '18rem',
+            textAlign: 'center',
           }}
         >
           נשמח לשמוע מכם בכל נושא שקשור למחשבון זה. ניתן ליצור קשר במייל:
           <a style={{ color: 'white' }} href="mailto:yslook7@gmail.com">
             yslook7@gmail.com
           </a>
-        </p>
+        </div>
       </div>
     </footer>
   )
