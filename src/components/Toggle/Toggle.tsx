1e7e9b93e2f900e85c9ee8d6595aa9829356bf33
Dor Wiser <vgibsonsg@gmail.com>
2024-01-27 23:30:22 +0200

    small changes

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,5 +1,5 @@
 import styles from './Toggle.module.css'
-
+import { motion } from 'framer-motion'
 export interface ToggleProps {
   active: boolean
   onChange: (name: string, active: boolean) => void
@@ -11,17 +11,23 @@ const Toggle = (props: ToggleProps) => {
   const { active, onChange, label, name } = props
 
   return (
-    <div
-      onClick={() => onChange(name, !active)}
-      className={styles.container}
-      style={{
-        color: active ? '#0066FF' : '#6F6F6F',
-        backgroundColor: active ? '#F2F6FD' : '#ffffff',
-        borderColor: active ? '#0066FF' : '#6F6F6F',
-      }}
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      transition={{ duration: 0.1 }}
     >
-      {label}
-    </div>
+      <div
+        onClick={() => onChange(name, !active)}
+        className={styles.container}
+        style={{
+          color: active ? '#0066FF' : '#6F6F6F',
+          backgroundColor: active ? '#F2F6FD' : '#ffffff',
+          borderColor: active ? '#0066FF' : '#6F6F6F',
+        }}
+      >
+        {label}
+      </div>
+    </motion.div>
   )
 }
 
