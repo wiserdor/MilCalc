29cc6c50eee0ee0aa49c227e378140bd1f8ed911
Dor Wiser <vgibsonsg@gmail.com>
2024-02-01 20:23:34 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn vite',
+    port: 5173,
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI,
+  },
 })
