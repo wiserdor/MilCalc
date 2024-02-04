c8af50455436194eb1079ba9d903d7f1c17eca84
Dor Wiser <vgibsonsg@gmail.com>
2024-02-04 17:45:01 +0200

    fix

--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -2,7 +2,7 @@ import styles from './Disclaimer.module.css'
 
 const Disclaimer = () => {
   const idfInfoLink =
-    'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/'
+    'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/'
   return (
     <div className={styles.disclaimer}>
       <div className={styles.disclaimerTitle}>
