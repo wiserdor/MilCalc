902ca100e0b02351d1cdab3273eeb4a91c1c323d
Dor Wiser <vgibsonsg@gmail.com>
2024-02-04 17:37:11 +0200

    fix link

--- src/data/compensation.ts
+++ src/data/compensation.ts
@@ -17,13 +17,13 @@ export const getApprovedItems = (
     name: 'מענק הוצאות אישיות',
     totalCompensation: totalWarPersonalExpenses,
     dateOfPayment: new Date('2023/11/10'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'מענק משפחה',
     totalCompensation: totalWarFamilyExpenses,
     dateOfPayment: new Date('2023/11/10'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'מענק הוצאות אישיות מוגדל',
@@ -31,19 +31,19 @@ export const getApprovedItems = (
     description:
       'ישולם בפעימה ראשונה עד ה- 14/1/24 (עבור השמ"פ שבוצע ב-2023), באופן אוטומטי ישירות לחשבון הבנק המדווח במערכות צה״ל.',
     dateOfPayment: new Date('2024/01/14'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'מענק משפחה מוגדל',
     totalCompensation: totalFromChildrenApproved,
     dateOfPayment: new Date('2024/01/14'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'מענק משפחה מיוחדת',
     totalCompensation: totalSpecialChildren,
     dateOfPayment: new Date('2024/03/01'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'התגמול הנוסף',
@@ -73,7 +73,7 @@ export const getApprovedItems = (
     name: totalMoreThan45 === 1250 ? 'מענק כלכלת בית' : 'מענק כלכלת בית מוגדל',
     totalCompensation: totalMoreThan45,
     dateOfPayment: new Date('2024/09/01'),
-    url: 'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    url: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
 ]
 
@@ -101,24 +101,21 @@ export const getApprovedNonPaidItems = (
     totalCompensation: totalVacation,
     description: `שובר חופשה למשרתי מילואים ששירתו לפחות 60 ימי שמ״פ במסגרת צו 8 ב'חרבות ברזל'. שימו לב, לא ניתן לקבל שובר זה יותר מפעם אחת. שובר אישי יישלח במהלך חציון ב׳ 2024 באופן אוטומטי כהודעה לטלפון הנייד של חייל המילואים, למימוש עד סוף שנת 2026.`,
     nonDirectMoney: true,
-    idfLink:
-      'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    idfLink: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'טיפול זוגי',
     totalCompensation: totalFamilyCare,
     description: `סיוע חד פעמי במימון טיפול זוגי למשרת מילואים ובן/בת הזוג ששירת 30 ימים ומעלה במסגרת צו 8 ב'חרבות ברזל'. הסיוע יינתן בכפוף לתצהיר + קבלה שיגיש משרת המילואים ולא יותר מהתשלום בפועל. הגשת קבלת החזר החל מ- 1.2.24 לאחר שליחת מסרון פרטני. לניצול עד סוף 2026. ניתן לממש רכיב זה ולשמור קבלות לטובת קבלת החזר.`,
     nonDirectMoney: true,
-    idfLink:
-      'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    idfLink: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   {
     name: 'טיפול רגשי, נפשי ומשלים',
     totalCompensation: totalMental,
     description: `סיוע חד פעמי במימון טיפול רגשי, נפשי, פסיכותרפי, משלים, אישי, זוגי או משפחתי למשרת מילואים ומשפחתו ששירת 30 ימים ומעלה במסגרת צו 8 'חרבות ברזל'.. הסיוע יינתן בכפוף לתצהיר + קבלה שיגיש משרת המילואים ולא יותר מהתשלום בפועל. הגשת קבלת החזר החל מ- 1.2.24 לאחר שליחת מסרון פרטני.לניצול עד סוף 2026. ניתן לממש רכיב זה ולשמור קבלות לטובת קבלת החזר.`,
     nonDirectMoney: true,
-    idfLink:
-      'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+    idfLink: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
   },
   ...(isIndependent
     ? [
@@ -146,8 +143,7 @@ export const getApprovedNonPaidItems = (
           totalCompensation: isCombat ? 11296 : 3388,
           description: `סיוע חד פעמי בתשלום שכר הלימוד עבור שנת הלימודים התשפ״ד, למשרתי מילואים שהינם סטודנטים במוסדות להשכלה גבוהה, במוסדות לאומנות ובמכללות הטכנולוגיות המאושרות ע״י מה״ט וביצעו 60 ימי שמ״פ במסגרת צו 8. מלגה זו תשולם באופן אוטומטי ע״י מופת ב-1.7.24 ישירות לחשבון הבנק המדווח במערכות צה״ל בכפוף להזנת ׳סטודנט׳ באזור האישי (תחת עדכון פרטים אישיים, מקצוע אזרחי - סטודנט) והצהרת משרת המילואים לאי קבלת מלגה/גובה המלגה שקיבל (פתיחת פניה למוקד המילואים).`,
           nonDirectMoney: true,
-          idfLink:
-            'https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/',
+          idfLink: 'https://www.miluim.idf.il/articles-list/מגוייסים-למילואים/',
         },
       ]
     : []),
