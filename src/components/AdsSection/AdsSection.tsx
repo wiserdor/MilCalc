5e460a5c6d8757d9b12b1d517de9dcb7ac00aca3
Dor Wiser <vgibsonsg@gmail.com>
2024-04-03 20:57:17 +0300

    familybiz

--- src/components/AdsSection/AdsSection.tsx
+++ src/components/AdsSection/AdsSection.tsx
@@ -1,7 +1,28 @@
 import RiseupBanner from "../RiseupBanner/RiseupBanner";
+import ReactGA from "react-ga4";
+
+const trackLinkClick = () => {
+  ReactGA.event({
+    category: "Link",
+    action: "familybiz_banner_click",
+    label: "familybiz_banner_click"
+  });
+};
 
 const AdsSection = () => {
-  return <RiseupBanner />;
+  return (
+    <div className="mb-2 flex flex-col gap-3">
+      <a
+        href="https://familybiz.onelink.me/kr4R/vhlaa7g0"
+        target="_blank"
+        rel="noreferrer"
+        onClick={trackLinkClick}
+      >
+        <img src="familybiz.png" alt="family biz" />
+      </a>
+      <RiseupBanner />
+    </div>
+  );
 };
 
 export default AdsSection;
